b995ba59122688818a8e789c005155cc3079d7aa
Thoma02 <tomi20020509@gmail.com>
2023-03-14 21:44:48 +0100

    Fetching images and names of cats, when clicking the circles or buttons

---
+++ index.js
@@ -0,0 +1,132 @@
+const main2 = document.getElementById("main-container-2");
+
+const myBengSelector = document.getElementById("bengalSelector");
+const myAbysSelector = document.getElementById("abysinianSelector");
+const myRagdSelector = document.getElementById("ragdollSelector");
+const myMainSelector = document.getElementById("mainSelector");
+
+const myLeftButton = document.getElementById("btn-left");
+const myRightButton = document.getElementById("btn-right");
+
+const catTitle = document.getElementById("cat-name");
+
+const getName = (i, data) => {
+    let name = data[i].name;
+    catTitle.innerText = name;
+}
+
+myBengSelector.addEventListener("click", function getBengalImage() {
+
+    fetch("https://api.thecatapi.com/v1/breeds")
+        .then((data) => data.json())
+        .then((data) => {
+            getName(10, data);
+        })
+
+    fetch("https://api.thecatapi.com/v1/images/search?limit=100&breed_ids=beng&api_key=live_y7qVxgAFuFGytLjRZccpbI5CGmv3LGoOm47AcaBmRs7EPYVzFEGrgYmFg2KppZTd")
+        .then((data) => data.json())
+        .then((data) => {
+            main2.style.backgroundImage = `url(${data[17].url})`;
+        })
+});
+
+
+myAbysSelector.addEventListener("click", function getAbyssinianImage() {
+
+    fetch("https://api.thecatapi.com/v1/breeds")
+        .then((data) => data.json())
+        .then((data) => {
+            getName(0, data);
+        })
+
+    fetch("https://api.thecatapi.com/v1/images/search?limit=100&breed_ids=abys&api_key=live_y7qVxgAFuFGytLjRZccpbI5CGmv3LGoOm47AcaBmRs7EPYVzFEGrgYmFg2KppZTd")
+        .then((data) => data.json())
+        .then((data) => {
+            main2.style.backgroundImage = `url(${data[12].url})`;
+        })
+})
+
+myRagdSelector.addEventListener("click", function getRagdImage() {
+
+    fetch("https://api.thecatapi.com/v1/breeds")
+        .then((data) => data.json())
+        .then((data) => {
+            getName(51, data);
+        })
+
+    fetch("https://api.thecatapi.com/v1/images/search?limit=100&breed_ids=ragd&api_key=live_y7qVxgAFuFGytLjRZccpbI5CGmv3LGoOm47AcaBmRs7EPYVzFEGrgYmFg2KppZTd")
+        .then((data) => data.json())
+        .then((data) => {
+            main2.style.backgroundImage = `url(${data[27].url})`;
+        })
+})
+
+myMainSelector.addEventListener("click", function getMainImage() {
+
+    fetch("https://api.thecatapi.com/v1/breeds")
+        .then((data) => data.json())
+        .then((data) => {
+            getName(40, data);
+        })
+
+    fetch("https://api.thecatapi.com/v1/images/search?limit=100&breed_ids=mcoo&api_key=live_y7qVxgAFuFGytLjRZccpbI5CGmv3LGoOm47AcaBmRs7EPYVzFEGrgYmFg2KppZTd")
+        .then((data) => data.json())
+        .then((data) => {
+            main2.style.backgroundImage = `url(${data[4].url})`;
+        })
+})
+
+const photosArrIDs = [17, 12, 27, 4];
+const photosArrURLs = ["https://api.thecatapi.com/v1/images/search?limit=100&breed_ids=beng&api_key=live_y7qVxgAFuFGytLjRZccpbI5CGmv3LGoOm47AcaBmRs7EPYVzFEGrgYmFg2KppZTd", 
+"https://api.thecatapi.com/v1/images/search?limit=100&breed_ids=abys&api_key=live_y7qVxgAFuFGytLjRZccpbI5CGmv3LGoOm47AcaBmRs7EPYVzFEGrgYmFg2KppZTd", 
+"https://api.thecatapi.com/v1/images/search?limit=100&breed_ids=ragd&api_key=live_y7qVxgAFuFGytLjRZccpbI5CGmv3LGoOm47AcaBmRs7EPYVzFEGrgYmFg2KppZTd", 
+"https://api.thecatapi.com/v1/images/search?limit=100&breed_ids=mcoo&api_key=live_y7qVxgAFuFGytLjRZccpbI5CGmv3LGoOm47AcaBmRs7EPYVzFEGrgYmFg2KppZTd"];
+const nameIDs = [10, 0, 51, 40];
+
+let counter = 0;
+
+myLeftButton.addEventListener("click", () => {
+
+    if(counter > 0 && counter <= 3) {
+        counter -= 1;
+    } else {
+        counter = 3;
+    } 
+
+    fetch("https://api.thecatapi.com/v1/breeds")
+        .then((data) => data.json())
+        .then((data) => {
+            let name = data[nameIDs[counter]].name;
+            catTitle.innerText = name;
+        })
+
+    fetch(`${photosArrURLs[counter]}`)
+        .then((data) => data.json())
+        .then((data) => {
+            main2.style.backgroundImage = `url(${data[photosArrIDs[counter]].url})`
+        })
+})
+
+myRightButton.addEventListener("click", () => {
+
+    if(counter >= 0 && counter < 3) {
+        counter += 1;
+    } else {
+        counter = 0
+    }
+
+    fetch("https://api.thecatapi.com/v1/breeds")
+        .then((data) => data.json())
+        .then((data) => {
+            let name = data[nameIDs[counter]].name;
+            catTitle.innerText = name;
+        })
+
+    fetch(`${photosArrURLs[counter]}`)
+        .then((data) => data.json())
+        .then((data) => {
+            main2.style.backgroundImage = `url(${data[photosArrIDs[counter]].url})`
+        })
+})
+
+
